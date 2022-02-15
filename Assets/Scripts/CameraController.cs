7ca24fa4455f1245c8c6989e120e83eb4c544191
contactparvendra <contactparvendra@gmail.com>
2022-02-15 00:19:06 -0500

    added character

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    private Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        
+    }
+}
