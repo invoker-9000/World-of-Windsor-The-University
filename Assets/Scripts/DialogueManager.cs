8e40e35a623bb916b73858b995e6516c927012bf
Parth Awari <awariparth97@gmail.com>
2022-02-16 17:19:32 -0500

    Added dialogue box to AluminiHall
    1) added character interaction with alumini hall to show the information about that building by pressing spacebar key near the building.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+    public bool dialogActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+        }
+    }
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+
+    }
+}
