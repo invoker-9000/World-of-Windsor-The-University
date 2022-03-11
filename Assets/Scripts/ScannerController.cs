268e2d2e88fdbd2eebb90df80cff93573fec8518
edwinemiko101 <edwinemiko.ee@gmail.com>
2022-03-11 16:42:12 -0500

    Quest3 Progress

---
+++ Assets/Scripts/ScannerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    static WebCamTexture frontCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (frontCam == null)
+        frontCam = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = frontCam;
+
+        if (!frontCam.isPlaying)
+        frontCam.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
