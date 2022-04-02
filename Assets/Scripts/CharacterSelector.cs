c062d7103cdabd9d2ca585ac8c25ff3b9bf03af6
Parvendra Singh <contactparvendra@gmail.com>
2022-04-02 18:04:57 -0400

    added music
    added music

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -6,6 +6,7 @@ using UnityEditor;
 
 public class CharacterSelector : MonoBehaviour
 {
+    public AudioSource selectionSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +21,14 @@ public class CharacterSelector : MonoBehaviour
 
     public void PlayGameMale()
     {
+        selectionSound.Play();
+
         PlayerController.playerCharacter = "Animations/MalePlayer/Player";
         SceneManager.LoadScene("MainGame");
     }
     public void PlayGameFemale()
     {
+        selectionSound.Play();
         PlayerController.playerCharacter = "Animations/FemalePlayer/F_Player_Front_view";
         SceneManager.LoadScene("MainGame");
     }
